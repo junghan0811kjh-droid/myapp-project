402a07360e48b68042af260ea833c47db73fd168
junghan <junghan0811kjh@gmail.com>
2026-06-30 12:24:33 +0900

    #1. deploy

--- backend/src/main/java/com/my/backend/dto/CreateUserRequest.java
+++ backend/src/main/java/com/my/backend/dto/CreateUserRequest.java
@@ -10,5 +10,4 @@ import lombok.ToString;
 public class CreateUserRequest {
     private String name;
     private String city;
-
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-// env_file을 명시하지 않으면 같은 폴더의 .env가 디폴트로 쓰임
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
